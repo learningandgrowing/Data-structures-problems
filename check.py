182ceb96251001c559a0befd213af85a5a723915
satyammishra001 <phycm10@gmail.com>
2019-09-07 09:39:47 +0530

    deleting some files

--- check.py
+++
@@ -1,7 +0,0 @@
-def reverse(li):
-    li.reverse()
-    
-        
-li = [1,2,3,4,5,6]
-reverse(li)
-print(li)
--- som.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 21 14:09:16 2019
-
-@author: satyam
-"""
-
-print('som')
